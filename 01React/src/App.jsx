e361a0625b5886fd72dc0670dc7aef3215cf5b20
Krishna-Khadka <KrishKhadka2059@gmail.com>
2023-11-21 17:34:30 +0545

    basic file structure learnt

--- 01React/src/App.jsx
+++ 01React/src/App.jsx
@@ -1,9 +1,13 @@
 
-
+import Greet from "./Greet"
 function App() {
 
   return (
+    <>
+    <Greet />
     <h1>Hello React JS</h1>
+    </>
+    
   )
 }
 
---
+++ 01React/src/Greet.jsx
@@ -0,0 +1,10 @@
+
+function Greet(){
+    return(
+        <>
+        <h1>Welcome Everyone | This is a greeting webpage</h1>
+        </>
+    )
+}
+
+export default Greet
